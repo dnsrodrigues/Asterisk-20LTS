e2752c7a1b34c3c4730ee92ca49b97a430c03be4
Denis Rodrigues <iran.dns@hotmail.com>
2023-09-21 14:11:50 -0300

    Update configuracao_de_arq_asterisk.sh

--- configuracao_de_arq_asterisk.sh
+++ configuracao_de_arq_asterisk.sh
@@ -34,6 +34,12 @@ tos=cs3
 cos=3
 '''
 
+#Dar um restart no Asterisk:
+'''
+asterisk -rvvv
+core restart now
+'''
+
 #Inserts para criar ramais:
 ->insert na tabela ps_aors:
 insert into ps_aors (id, contact, default_expiration, mailboxes, max_contacts, minimum_expiration, remove_existing, qualify_frequency, authenticate_qualify, maximum_expiration, outbound_proxy, support_path, qualify_timeout, voicemail_extension, remove_unavailable) values ('1001', null, 7200, null, 1, 60, 'yes', 20, 'no', 7200, null, 'no', null, null, 'yes');
